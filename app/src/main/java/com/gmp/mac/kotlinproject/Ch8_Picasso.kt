a72c1bb218a468dd576c7f412e10c35489bf1f83
이준구 <wnsrn403@codeartist.co.kr>
2018-05-12 17:19:23 +0900

    Kotlin Howl Ch8

---
+++ app/src/main/java/com/gmp/mac/kotlinproject/Ch8_Picasso.kt
@@ -0,0 +1,70 @@
+package com.gmp.mac.kotlinproject
+
+import android.media.Image
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import android.widget.GridLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
+import com.gmp.mac.kotlinproject.Ch7_DataModel.PapagoDTO
+import com.google.gson.Gson
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_picasso.*
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
+
+
+//Code -> Convert JavaFile to Kotlin File
+
+class Ch8_Picasso : AppCompatActivity() {
+
+
+    val imageUrls = arrayOf(
+            "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&h=350",
+            "https://images.pexels.com/photos/35807/rose-red-rose-romantic-rose-bloom.jpg?auto=compress&cs=tinysrgb&h=350",
+            "https://images.pexels.com/photos/56866/garden-rose-red-pink-56866.jpeg?auto=compress&cs=tinysrgb&h=350",
+            "https://images.pexels.com/photos/86243/pexels-photo-86243.jpeg?auto=compress&cs=tinysrgb&h=350"
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_picasso)
+
+
+        recyclerView.adapter = MyRecyclerviewAdapter()
+        recyclerView.layoutManager = GridLayoutManager(this,2)
+
+    }
+
+    inner class MyRecyclerviewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+           var imageView = ImageView(this@Ch8_Picasso)
+            var params = LinearLayout.LayoutParams(parent!!.measuredWidth/2,parent!!.measuredHeight/2)
+            imageView.layoutParams = params
+            imageView.scaleType = ImageView.ScaleType.FIT_XY
+            return CustomHolder(imageView)
+        }
+
+        inner class CustomHolder(imageView: ImageView):RecyclerView.ViewHolder(imageView){
+
+        }
+
+        override fun getItemCount(): Int {
+            return imageUrls.size
+        }
+
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+          Picasso.get().load(imageUrls[position]).into(holder!!.itemView as ImageView)
+        }
+
+    }
+
+
+
+}
--- app/src/main/java/com/gmp/mac/kotlinproject/MainActivity.kt
+++ app/src/main/java/com/gmp/mac/kotlinproject/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.gmp.mac.kotlinproject
 
+import android.media.Image
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import android.widget.GridLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
 import com.gmp.mac.kotlinproject.Ch7_DataModel.PapagoDTO
 import com.google.gson.Gson
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_picasso.*
 import okhttp3.*
 import org.json.JSONObject
 import java.io.IOException
@@ -14,52 +23,14 @@ import java.io.IOException
 class MainActivity : AppCompatActivity() {
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Papago okhttp
-        val JSON = MediaType.parse("application/json; charset=utf-8")
 
-        val client = OkHttpClient()
-        var url = "https://openapi.naver.com/v1/papago/n2mt";
-        var json = JSONObject();
-        json.put("source","ko")
-        json.put("target","en")
-        json.put("text","만나서 반갑습니다.")
-
-
-        //Api Id,Secret header add
-        val body = RequestBody.create(JSON, json.toString())
-        val request = Request.Builder()
-                .header("X-Naver-Client-Id","AparHsJGIZeOSTE9K37a")
-                .addHeader("X-Naver-Client-Secret","5LdrNko0b4")
-                .url(url)
-                .post(body)
-                .build()
-
-        //Callback
-        client.newCall(request).enqueue(object : Callback{
-            override fun onFailure(call: Call?, e: IOException?) {
-                println(e.toString());
-
-            }
-
-            override fun onResponse(call: Call?, response: Response?) {
-
-                //Json result
-                var str =  response!!.body()!!.string()
-                println(str);
-
-                //Papago DTO -> Nice to meet you
-                var papagoDTO = Gson().fromJson<PapagoDTO>(str, PapagoDTO::class.java)
-                println(papagoDTO.message!!.result!!.translatedText)
-            }
-
-        })
 
     }
+
 
 
 
